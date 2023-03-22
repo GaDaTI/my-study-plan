eab61df96ccdaa638bc2600c1f4e0d46a627ebfa
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-21 21:10:27 -0300

    Corrigindo erro na variavel

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@ lista = [ 5, 1, 4, 2, 8]
 
 for indice in range(len(lista)):
       number_more = max(lista[indice],  lista[indice+1])
-      index_do_mais = lista.index(maior)
+      index_do_mais = lista.index(number_more)
 
 
 
